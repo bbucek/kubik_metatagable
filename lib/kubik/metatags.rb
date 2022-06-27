d37847ea56c1d539e7ca45bef9c0d85ce3984b7e
Bart O <bart@primate.co.uk>
2022-06-27 22:54:24 +0100

    added sample app and some files jugling

--- lib/kubik/metatags.rb
+++ lib/kubik/metatags.rb
@@ -12,11 +12,11 @@ module Kubik
         meta_description: meta_tags.try(:meta_description),
         og_title: meta_tags.try(:og_title),
         og_description: meta_tags.try(:og_description),
-        og_image: meta_tags.try(:og_image),
+        og_image: meta_tags.try(:og_image_upload) || meta_tags.try(:og_image),
         og_url: request.original_url,
         twitter_title: meta_tags.try(:twitter_title),
         twitter_description: meta_tags.try(:twitter_description),
-        twitter_image: meta_tags.try(:twitter_media),
+        twitter_image: meta_tags.try(:twitter_image_upload) || meta_tags.try(:twitter_media),
         twitter_card_type: meta_tags.try(:twitter_card_type)
       }
     end
